adc931c6c21aac1153c61b4ef2c07e1b19b50efc
mohammed-yaseen118 <yaseenhulgur@gmail.com>
2025-05-06 14:30:29 +0530

    Employee Branch

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Amit", "15-08-1995", "E001", 45000);
+        emp.displayEmployeeDetails();
+    }
+}
